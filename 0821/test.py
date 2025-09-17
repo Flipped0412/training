0924aab9d29b6a484fe1560a9db1f4a940ea8063
unknown <flipped0412@naver.com>
2025-09-17 17:56:52 +0900

    0917

--- 0821/test.py
+++ 0821/test.py
@@ -1,15 +1,24 @@
-# arr = list(map(int, input().split()))
+# # arr = list(map(int, input().split()))
+# #
+# # print(arr.pop(0), arr)
+# # print(arr.pop(0), arr)
+# # print(arr.pop(0), arr)
 #
-# print(arr.pop(0), arr)
-# print(arr.pop(0), arr)
-# print(arr.pop(0), arr)
+# nodes = [list(map(int, input().split())) for _ in range(5)]
+# print(nodes)
+# node_lst = []
+#
+# for i in range(len(nodes)):
+#     for j in range(2):
+#         node_lst.append(nodes[i][j])
+#
+# print(node_lst)
 
-nodes = [list(map(int, input().split())) for _ in range(5)]
-print(nodes)
-node_lst = []
+# N = 5
+# print([[0]*N for _ in range(N)])
 
-for i in range(len(nodes)):
-    for j in range(2):
-        node_lst.append(nodes[i][j])
-
-print(node_lst)
+# queue = [1]
+# if queue:
+#     print("에")
+# elif not queue:
+#     print("아")
